b4999d2b9833923d7c8875e440b09ab69e110aa6
Wakil Ahmad <wakilahmadhamidi24@gmail.com>
2023-12-07 18:53:04 +0530

    Add files via upload

---
+++ Zoo Project in Python.py
@@ -0,0 +1,319 @@
+# This is a program named "Zoo" that allows the user to view different animals in the zoo.
+# It will display a menu of different categories of animals (mammals, birds, reptiles, fish, etc.).
+# The user will be able to select a category and then an animal from that category.
+# The program will then display the ASCII art for that animal.
+
+# ASCII art is a graphic design technique that uses computers for presentation and consists of pictures pieced together from individual characters.
+
+# You may contribute to this program by adding more animals and ASCII art for those animals.
+Lion = r"""
+
+                                               ,w.
+                                             ,YWMMw  ,M  ,
+                        _.---.._   __..---._.'MMMMMw,wMWmW,
+                   _.-""        '''           YP"WMMMMMMMMMb,
+                .-' __.'                   .'     MMMMW^WMMMM;
+    _,        .'.-'"; `,       /`     .--""      :MMM[==MWMW^;
+ ,mM^"     ,-'.'   /   ;      ;      /   ,       MMMMb_wMW"  @\
+,MM:.    .'.-'   .'     ;     `\    ;     `,     MMMMMMMW `"=./`-,
+WMMm__,-'.'     /      _.\      F'''-+,,   ;_,_.dMMMMMMMM[,_ / `=_}
+"^MP__.-'    ,-' _.--""   `-,   ;       \  ; ;MMMMMMMMMMW^``; __|
+           /   .'            ; ;         )  )`{  \ `"^W^`,   \  :
+          /  .'             /  (       .'  /     Ww._     `.  `"
+         /  Y,              `,  `-,=,_{   ;      MMMP`""-,  `-._.-,
+        (--, )                `,_ / `) \/"")      ^"      `-, -;"\:
+"""
+
+Deer = r"""
+   /|       |\
+`__\\       //__'
+   ||      ||
+ \__`\     |'__/
+   `_\\   //_'
+   _.,:---;,._
+   \_:     :_/
+     |@. .@|
+     |     |
+     ,\.-./ \
+     ;;`-'   `---__________-----.-.
+     ;;;                         \_\
+     ';;;                         |
+      ;    |                      ;
+       \   \     \        |      /
+        \_, \    /        \     |\
+          |';|  |,,,,,,,,/ \    \ \_
+          |  |  |           \   /   |
+          \  \  |           |  / \  |
+           | || |           | |   | |
+           | || |           | |   | |
+           | || |           | |   | |
+           |_||_|           |_|   |_|
+          /_//_/           /_/   /_/
+"""
+
+Camel = r"""
+ ___.-''''-.
+/___  @    |
+',,,,.     |         _.'''''''._
+     '     |        /           \
+     |     \    _.-'             \
+     |      '.-'                  '-.
+     |                               ',
+     |                                '',
+      ',,-,                           ':;
+           ',,| ;,,                 ,' ;;
+              ! ; !'',,,',',,,,'!  ;   ;:
+             : ;  ! !       ! ! ;  ;   :;
+             ; ;   ! !      ! !  ; ;   ;,
+            ; ;    ! !     ! !   ; ;
+            ; ;    ! !    ! !     ; ;
+           ;,,      !,!   !,!     ;,;
+           /_I      L_I   L_I     /_I
+"""
+
+Goose = r"""
+                                    _
+                                ,-"" "".
+                              ,'  ____  `.
+                            ,'  ,'    `.  `._
+   (`.         _..--.._   ,'  ,'        \    \
+  (`-.\    .-""        ""'   /          (  d _b
+ (`._  `-"" ,._             (            `-(   \
+ <_  `     (  <`<            \              `-._\
+  <`-       (__< <           :
+   (__        (_<_<          ;
+    `------------------------------------------
+"""
+
+Rabbit = r"""
+         ,
+        /|      __
+       / |   ,-~ /
+      Y :|  //  /
+      | jj /( .^
+      >-"~"-v"
+     /       Y
+    jo  o    |
+   ( ~T~     j
+    >._-' _./
+   /   "~"  |
+  Y     _,  |
+ /| ;-"~ _  l
+/ l/ ,-"~    \
+\//\/      .- \
+ Y        /    Y
+ l       I     !
+ ]\      _\    /"\
+(" ~----( ~   Y.  )
+"""
+
+Bat = r"""
+_________________               _________________
+ ~-.              \  |\___/|  /              .-~
+     ~-.           \ / o o \ /           .-~
+        >           \\  W  //           <
+       /             /~---~\             \
+      /_            |       |            _\
+         ~-.        |       |        .-~
+            ;        \     /        i
+           /___      /\   /\      ___\
+                ~-. /  \_/  \ .-~
+                   V         V
+"""
+
+Horse = r"""
+      ,  ,.~"'"'"~~..
+  )\,)\`-,       `~._                                     .--._
+  \  \ | )           `~._                   .-""'""--_   /     `.
+ _/ ('  ( _(\            `~~,__________..-"'          `-&lt;      \
+ )   )   `   )/)   )        \                            \,-.     |
+') /)`      \` \,-')/\      (                             \ /     |
+(_(\ /7      |.   /'  )'  _(`                              Y      |
+    \       (  `.     ')_/`                                |      /
+     \       \   \                                         |)    (
+      \ _  /\/   /                                         (      `~.
+       `-._)     |                                        / \        `,
+                 |                          |           .'   )      (`
+                 \                        _,\          /     \_    (`
+                  `.,      /       __..'7"   \         |       )  (
+                  .'     _/`-..--""      `.   `.        \      `._/
+                .'    _.j     /            `-.  `.       \
+              .'   _.'   \    |               `.  `.      \
+             |   .'       ;   ;               .'  .'`.     \
+             \_  `.       |   \             .'  .'   /    .'
+               `.  `-, __ \   /           .'  .'     |   (
+                 `.  `'` \|  |           /  .-`     /   .'
+                   `-._.--t  ;          |_.-)      /  .'
+                          ; /           \  /      / .'
+                         / /             `'     .' /
+                        /,_\                  .',_(
+                       /___(                 /___(
+"""
+# Add Animals ASCII ART after each animal name within triple quotes
+Monkey = r"""
+        To be added soon!
+"""
+Elephant = r"""
+        To be added soon!
+"""
+Giraffe = r"""
+        To be added soon!
+"""
+Eagle = r"""
+        To be added soon!
+"""
+Owl = r"""
+        To be added soon!
+"""
+Parrot = r"""
+        To be added soon!
+"""
+Flamingo = r"""
+        To be added soon!
+"""
+Ostrich = r"""
+        To be added soon!
+"""
+Turkey = r"""
+        To be added soon!
+"""
+Duck = r"""
+        To be added soon!
+"""
+Macaw = r"""
+        To be added soon!
+"""
+Canary = r"""
+        To be added soon!
+"""
+# Add more animals here
+
+print("===========================")
+print("Welcome to Afghanistan Zoo!")
+print("===========================")
+
+while True:
+    print("1. Mammals")
+    print("2. Birds")
+    print("3. Reptiles")
+    print("4. Fish")
+    print("5. Exit")
+    category = input("Select the category of animals you would like to see: ")
+    if category == '1':
+        while True:
+            print("1. Lion")
+            print("2. Deer")
+            print("3. Camel")
+            print("4. Goose")
+            print("5. Rabbit") 
+            print("6. Bat")
+            print("7. Monkey")
+            print("8. Elephant")
+            print("9. Giraffe")
+            print("10. Exit")
+
+            animal = input("Select the animal you would like to see: ")
+            if animal == '1':
+                print("Let's see the Lion")
+                print(Lion)
+            elif animal == '2':
+                print("Let's see the Deer")
+                print(Deer)
+            elif animal == '3':
+                print("Let's see the Camel")
+                print(Camel)
+            elif animal == '4':
+                print("Let's see the Goose")
+                print(Goose)
+            elif animal == '5':
+                print("Let's see the Rabbit")
+                print(Rabbit)
+            elif animal == '6':
+                print("Let's see the Bat")
+                print(Bat)
+            elif animal == '7':
+                print("Let's see the Monkey")
+                print(Monkey)
+            elif animal == '8':
+                print("Let's see the Elephant")
+                print(Elephant)
+            elif animal == '9':
+                print("Let's see the Giraffe")
+                print(Giraffe)
+            elif animal == '10':
+                print("Thanks for visiting the Zoo!")
+                print("See you later!")
+                break
+            else:
+                print("Please enter a valid number")
+    elif category == '2':
+        
+        while True:
+            print("Select the animal you would like to see:")
+            print("1. Eagle")
+            print("2. Owl")
+            print("3. Parrot")
+            print("4. Flamingo")
+            print("5. Ostrich")
+            print("6. Turkey")
+            print("7. Duck")
+            print("8. Macaw")
+            print("9. Canary")
+            print("10. Exit")
+
+            animal = input("Select the animal you would like to see: ")
+            if animal == '1':
+                print("Let's see the Eagle")
+                print(Eagle)
+            elif animal == '2':
+                print("Let's see the Owl")
+                print(Owl)
+            elif animal == '3':
+                print("Let's see the Parrot")
+                print(Parrot)
+            elif animal == '4':
+                print("Let's see the Flamingo")
+                print(Flamingo)
+            elif animal == '5':
+                print("Let's see the Ostrich")
+                print(Ostrich)
+            elif animal == '6':
+                print("Let's see the Turkey")
+                print(Turkey)
+            elif animal == '7':
+                print("Let's see the Duck")
+                print(Duck)
+            elif animal == '8':
+                print("Let's see the Macaw")
+                print(Macaw)
+            elif animal == '9':
+                print("Let's see the Canary")
+                print(Canary)
+            elif animal == '10':
+                print("Thanks for visiting the Zoo!")
+                print("See you later!")
+                break
+            else:
+                print("Please enter a valid number")
+    elif category == '3':
+        print("To be Added Soon!")
+        # print("1. Snake")
+        # print("2. Crocodile")
+        # print("3. Tortoise")
+        # print("4. Lizard")
+        # print("5. Exit")
+    
+    elif category == '4':
+        print("To be Added Soon!")
+        # print("1. Shark")
+        # print("2. Goldfish")
+        # print("3. Clown Fish")
+        # print("4. Octopus")
+        # print("5. Exit")
+        
+    elif category == '5':
+        print("Thanks for visiting the Zoo!")
+        print("See you later!")
+        break
+    else:
+        print("Please enter a valid number")
